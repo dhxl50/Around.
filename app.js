6723691a81cd49af58cdde5f8e7a1dc115bc5b65
dhxl50 <dhxl50@gmail.com>
2021-07-21 10:29:00 +0000

    feat: Express 정적파일 연결 및 Grid를 이용한 레이아웃 구성

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const clientDir = "/client"
 const app = express();
 
+app.use('/public',express.static(__dirname+'/public'));	//express 정적파일 연결
+
 app.get('',(req,res)=>{
-	res.sendFile(__dirname + "/client/index.html");
+	res.sendFile(__dirname + "/public/index.html");
 })
 
 app.listen(8000,()=>{
