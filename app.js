f3ac46bcad75ca93cc2755086cc599b1504e664c
dhxl50 <dhxl50@gmail.com>
2021-08-01 11:19:56 +0000

    feat: Ejs와 TailwindCss 적용

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
+const path = require('path');
 const express = require('express');
 const app = express();
 
-app.use('/public',express.static(__dirname+'/public'));	//express 정적파일 연결
 
-app.get('',(req,res)=>{
-	res.sendFile(__dirname + "/public/index.html");
+app.set('views',path.join(__dirname,'views'));
+app.set('view engine','ejs');					//view engine으로 ejs 사용
+app.use(express.static(__dirname+'/public'));	//express 정적파일 연결
+
+app.get('/',(req,res)=>{
+	res.render('index');
 })
 
 app.listen(8000,()=>{
